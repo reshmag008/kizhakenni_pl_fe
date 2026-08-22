4ebe9e896397f14ca505a9a8484e32b3337b75a9
Reshma G <reshma.g@cdg.ws>
2026-08-22 14:50:55 +0530

    bug fix

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
 export const BACKEND_URL =  "https://kizhakkeni-backend-204746249106.europe-west1.run.app/"   //  "https://my-node-app-204746249106.asia-south1.run.app/" //  ; 
-export const TOTAL_PLAYER = 9
+export const TOTAL_PLAYER = 11
 
 
 
